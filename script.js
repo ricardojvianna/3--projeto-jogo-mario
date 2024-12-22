9dc31d969d9ed4618fe47c35e54cf0d626efbab9
ricardojvianna <comunicacao.vianna@gmail.com>
2024-12-22 01:16:32 -0300

    Implementação do JavaScript no cógido

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Estou linkado!")
+// const mario = document.querySelector(".mario")
+// const pipe = document.querySelector(".pipe")
+// const restart = document.querySelector(".restart")
+// console.log(mario, pipe, restart)
+
+//  Todos os elementos do Html estavam selecionados, mas ele fez de um
+//  outro jeito, usando um array e uma aerofunction.
+
+// const [mario, pipe, restart] = [".mario", ".pipe", ".restart"].map(item => {
+//     document.querySelector(item)
+//  }) 
+// -> ele fez assim,  mas deu undefined e ai ele optou por deixar desse 
+// abaixo.
+
+
+const mario = document.querySelector(".mario")
+
+const jump = () => {
+    mario.classList.add("jump")
+    setTimeout(() => {
+        mario.classList.remove("jump")
+    }, 500)
+    
+}
+
+document.addEventListener("keydown", jump)
